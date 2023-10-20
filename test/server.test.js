66c3bde67ad6f9bf8b7bf3596376cef063ba8b0d
Areli Mendoza Perez <63635995+arelimdz@users.noreply.github.com>
2023-10-21 10:52:34 +1100

    feat: test server

---
+++ test/server.test.js
@@ -0,0 +1,17 @@
+// Test the routes from server.js
+const {app} = require('../src/server');
+
+// Import supertest so we can manage the app/server in tests properly
+const request = require('supertest');
+
+describe("Server root route exists and returns hello world", () => {
+	test("Root route exists and returns status 200", async() => {
+        const responseResult = await request(app).get("/");
+        expect(responseResult.statusCode).toEqual(200);
+	});
+
+	test("Root route exists and returns hello world message", async() => {
+        const response = await request(app).get("/");
+		expect(response.body.message).toEqual("Hello World!");
+	});
+});
